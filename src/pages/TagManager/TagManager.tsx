3d0c36c187af2e3bfbe2d48d3c60c4944b1673b2
PhongPT <phongzzz98@gmail.com>
2022-07-03 02:19:09 +0700

    update tag table mng

--- src/pages/TagManager/TagManager.tsx
+++ src/pages/TagManager/TagManager.tsx
@@ -1,6 +1,6 @@
 import { Row, Col, Tag, Table, Input, Button } from 'antd'
 import { AdminInfoBlock } from '../../common/Admin/AdminInfoBlock/AdminInfoBlock'
-import { TagsFilled, EditOutlined, DeleteFilled, ProfileFilled } from '@ant-design/icons';
+import { TagsFilled, EditOutlined, DeleteFilled, ProfileFilled, ProjectOutlined, PlusOutlined } from '@ant-design/icons';
 import './TagManagerStyle.css'
 import { useEffect } from 'react';
 import { ApplicationDispatch } from '../../store/store';
@@ -26,13 +26,15 @@ export const TagManager = () => {
     dispatch(getAllPostAction())
   }, [dispatch])
 
+  const defaultFooter = () => <Button style={{width: '100%'}} icon={<PlusOutlined/>} type='dashed'>Tạo thẻ mới</Button>;
+
   return (
     <div className='tag-manager-page'>
       <h1>Quản lý thẻ</h1>
       <Row gutter={[20, 20]}>
         <Col span={8} ><AdminInfoBlock description='Tổng số thẻ' quantity={`${cloneTagList.length}`} icon={<TagsFilled />} /></Col>
         <Col span={8} ><AdminInfoBlock description='Số bài viết dùng thẻ' quantity={`${postUseTag}/${clonePostList.length}`} icon={<ProfileFilled />} /></Col>
-        <Col span={8} ><AdminInfoBlock description='Thẻ nổi bật' componentInfo={<div className='popular-tags'>{popularTag.map((tag) => <Tag style={{ borderRadius: '2em', fontSize: '0.9rem' }} color={tag.colorcode}>{`${tag.content}: ${tag.postusetag}`}</Tag>)}</div>} icon={<TagsFilled />} /></Col>
+        <Col span={8} ><AdminInfoBlock description='Thẻ nổi bật' componentInfo={<div className='popular-tags'>{popularTag.map((tag) => <Tag style={{ borderRadius: '2em', fontSize: '0.9rem' }} color={tag.colorcode}>{`${tag.content}: ${tag.postusetag}`}</Tag>)}</div>} icon={<ProjectOutlined />} /></Col>
       </Row>
       <h5 style={{marginTop: 15}}>Danh sách thẻ</h5>
       <div className="tag-table-container">
@@ -46,11 +48,15 @@ export const TagManager = () => {
             pageSize: 8,
           }}
           size='large'
-          bordered={true}>
-          <Table.Column title="Tên thẻ" render={(tag: ITag) => <span>{tag.content}</span>} width="15%" />
-          <Table.Column title="Mô tả" render={(tag: ITag) => <span>{tag.description}</span>} width="55%" />
-          <Table.Column title="Icon" render={(tag: ITag) => <i style={{fontSize: '2em'}} className={tag.icon_class}></i>} width="5%" />
-          <Table.Column title="Mã màu" render={(tag: ITag) => <Tag color={tag.colorcode} style={{padding: 3, borderRadius: '2em'}}>{tag.colorcode}</Tag>} width="15%" />
+          bordered={true}
+          rowKey={(tag: ITag) => tag.id}
+          sticky={true}
+          footer={defaultFooter}
+          >
+          <Table.Column title="Tên thẻ" render={(tag: ITag) => <span>{tag.content}</span>} width="10%" />
+          <Table.Column title="Mô tả" render={(tag: ITag) => <span>{tag.description}</span>} width="60%" />
+          <Table.Column title="Icon" render={(tag: ITag) => <i style={{fontSize: '2em'}} className={tag.icon_class}></i>} width="7%" />
+          <Table.Column title="Mã màu" render={(tag: ITag) => <Tag color={tag.colorcode} style={{padding: 3, borderRadius: '2em'}}>{tag.colorcode}</Tag>} width="13%" />
           <Table.Column className="tag-mng-action" title="Hành động" width="10%"
             render={() => (
               <>
